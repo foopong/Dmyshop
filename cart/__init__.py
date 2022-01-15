7fe2e26eb6ce8a889874086bcae18dc3b69b064b
rompo <foopong2000@yahoo.com>
2022-01-15 21:45:21 +0700

    Initial commit

---
+++ cart/__init__.py
@@ -0,0 +1,24 @@
+class Cart(object):
+    def __init__(self, request):
+        # ...
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) \
+                    * self.get_total_price()
+
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
